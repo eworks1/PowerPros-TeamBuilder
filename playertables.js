ff891284968650c51e1f324179fb1bfeda8508d1
Edon <edon@valdman.works>
2023-03-22 12:41:18 -0400

    Create playertables.js
    Initial version

---
+++ playertables.js
@@ -0,0 +1,12 @@
+import { players } from "./players.json" assert { type: "json" };
+
+const playerPoolTable = document.getElementById('player-pool-table');
+if (playerPoolTable) {
+    players.forEach(player => {
+        const tableRow = document.createElement('tr');
+        const tableData = document.createElement('td');
+        tableData.innerHTML = `<span>${player.Name}</span> <span>${player["Point Cost"]}</span>`
+        tableRow.appendChild(tableData);
+        playerPoolTable.appendChild(tableRow);
+    })
+}
