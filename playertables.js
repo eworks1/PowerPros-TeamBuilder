c8c733cdffe45cf29aed86295215d91f435ff224
Edon <edon@valdman.works>
2023-03-22 13:42:01 -0400

    Update playertables.js
    Refactored code to add the players into 2 columns per row.

--- playertables.js
+++ playertables.js
@@ -2,11 +2,23 @@ import players from "./getPlayers.js";
 
 const playerPoolTable = document.getElementById('player-pool-table');
 if (playerPoolTable) {
-    players.forEach(player => {
-        const tableRow = document.createElement('tr');
-        const tableData = document.createElement('td');
-        tableData.innerHTML = `<span>${player.Name}</span> <span>${player["Point Cost"]}</span>`
-        tableRow.appendChild(tableData);
-        playerPoolTable.appendChild(tableRow);
+    players.forEach((player, i) => {
+        if (i % 2 == 0) {
+            // first item of row
+            const tableRow = document.createElement('tr');
+            const cellData = document.createElement('td');
+            cellData.innerHTML = `<span>${player.Name}</span> <span>${player["Point Cost"]}</span>`;
+            tableRow.appendChild(cellData);
+            playerPoolTable.appendChild(tableRow);
+        } else {
+            // second item of row
+            const rowElements = playerPoolTable.getElementsByTagName('tr');
+            const lastRow = rowElements.item(rowElements.length - 1);
+
+            const cellData = document.createElement('td');
+            cellData.innerHTML = `<span>${player.Name}</span> <span>${player["Point Cost"]}</span>`;
+            lastRow.appendChild(cellData);
+        }
+
     })
 }
