7674e3a93acc6e468a993903f0a7b327c8b4d2dd
Edon <edon@valdman.works>
2023-03-25 11:24:16 -0400

    Update select-player.js
    Started adding code to `playerClicked()` function.

--- assets/js/select-player.js
+++ assets/js/select-player.js
@@ -1,4 +1,9 @@
 function playerClicked(player) {
     // update all 3 detail views (just fielding for now)
+    const fieldingDetailView = document.getElementById('fielding-detail-view');
 
     const playerNameBox = fieldingDetailView.getElementsByClassName('player-name-box').item(0);
+    if (playerNameBox) {
+        playerNameBox.textContent = player['Name Abbreviation'];
+    }
+}
