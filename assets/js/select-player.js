d844000cf29d99b186844b4a15c5973979122da0
Edon <edon@valdman.works>
2023-03-25 12:18:28 -0400

    Update select-player.js
    Replaced call to update text of `playerNameBox` with call to `updatePlayerNameBox`.

--- assets/js/select-player.js
+++ assets/js/select-player.js
@@ -4,6 +4,10 @@ function playerClicked(player) {
 
     const playerNameBox = fieldingDetailView.getElementsByClassName('player-name-box').item(0);
     if (playerNameBox) {
-        playerNameBox.textContent = player['Name Abbreviation'];
+        updatePlayerNameBox(
+            playerNameBox,
+            player['Name Abbreviation'],
+            player['Field Position']
+        );
     }
 }
