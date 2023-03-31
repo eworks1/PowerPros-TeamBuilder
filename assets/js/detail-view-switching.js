494333a99e34cb6162634d8b6f81a14802ed5d24
Edon <edon@valdman.works>
2023-03-30 22:57:11 -0400

    Update detail-view-switching.js
    Added to the `#detail-view` page navigation.

--- assets/js/detail-view-switching.js
+++ assets/js/detail-view-switching.js
@@ -1,7 +1,39 @@
+var currentDetailPageNum = 0;
+const detailPageMax = 0;
+const detailPageViewIDs = [
+    'fielding-detail'
+];
+
 function leftDetailButtonClicked() {
-    console.log('Left detail button clicked');
+    currentDetailPageNum -= 1;
+    if (currentDetailPageNum < 0) {
+        currentDetailPageNum = detailPageMax
+    }
+
+    console.log('Left detail button clicked: ', currentDetailPageNum);
+    updateDetailPageVisibility();
 }
 
 function rightDetailButtonClicked() {
-    console.log('Right detail button clicked');
+    currentDetailPageNum += 1;
+    if (currentDetailPageNum > detailPageMax) {
+        currentDetailPageNum = 0
+    }
+
+    console.log('Right detail button clicked', currentDetailPageNum);
+    updateDetailPageVisibility();
+}
+
+function updateDetailPageVisibility() {
+    detailPageViewIDs.forEach((id, i) => {
+        if (i == currentDetailPageNum) {
+            const currentPage = document.getElementById(id);
+            currentPage.classList.add('current-detail-page');
+            currentPage.classList.remove('hidden-detail-page');
+        } else {
+            const otherPage = document.getElementById(id);
+            otherPage.classList.remove('current-detail-page');
+            otherPage.classList.add('hidden-detail-page');
+        }
+    })
 }
