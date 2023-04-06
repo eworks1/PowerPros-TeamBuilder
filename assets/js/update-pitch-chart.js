df862ca2c5d080ae26dbe87b0da4c76a736c8b28
Edon <edon@valdman.works>
2023-04-06 15:08:49 -0500

    Create update-pitch-chart.js
    Created start of `updatePitchChart()` JS.

---
+++ assets/js/update-pitch-chart.js
@@ -0,0 +1,9 @@
+// assume right is inside, left is outside, if pitcher throws L => mirror horizontally 
+
+/**
+ * @this {}
+ * @param {string[]} pitches 
+ */
+function updatePitchChart(pitches) {
+    
+}
