07ed9a6440a26ff7d95d3709ab81fcafbf10ae3a
Edon <edon@valdman.works>
2023-04-07 12:12:46 -0500

    Update team-management.js
    Fixed space before dash.

--- assets/js/team-management.js
+++ assets/js/team-management.js
@@ -17,7 +17,7 @@ var team = {
 /**
  * @param {any} player
  */
-{% - endraw -%}
+{%- endraw -%}
 function rightClicked(player) {
     // If they're on the team, they'll be removed here.
     const deleted = removePlayer(player);
