ddd7b8dbad3e1051fd798ce743e4baa5d55c3d82
Edon <edon@valdman.works>
2023-05-08 23:44:39 -0400

    Update all-pitches.js
    Added typings to `all-pitches` data.

--- assets/js/all-pitches.js
+++ assets/js/all-pitches.js
@@ -1,11 +1,23 @@
 ---
 ---
 
+{% raw %}
+/** 
+ * @typedef {"Up 1"|"Up 2"|"Inside"|"Down-Inside"|"Down"|"Down-Outside"|"Outside"} PitchDirection
+ */
+
+/**
+ * @typedef {Object} Pitch
+ * @property {string} Name
+ * @property {string} Abbreviation
+ * @property {PitchDirection} Direction
+ */
+
+/** @type {Object.<string, Pitch>} */
 const all_pitches = allPitches().pitches;
 
-{% raw %}
 /**
- * @returns { { pitches: Object }}
+ * @returns {{ pitches: Object.<string, Pitch> }}
  */
 function allPitches() {
 {% endraw %}
