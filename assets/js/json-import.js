f8bdb63a47e703f62890e533167eb4240c17008c
Edon <edon@valdman.works>
2023-05-10 10:57:23 -0400

    Moved `json-import` JS to separate file.

---
+++ assets/js/json-import.js
@@ -0,0 +1,21 @@
+/** @type {HTMLTextAreaElement} */
+const jsonBox = document.getElementById('json-text-box');
+function generateJson(event) {
+    const teamJSONStr = JSON.stringify(team);
+    jsonBox.value = teamJSONStr;
+}
+
+/**
+ * @param {Event} event
+ */
+async function copyJson(event) {
+    return navigator.clipboard.writeText(jsonBox.value).then(() => {
+        event.target.innerText = 'Copied!';
+        return new Promise(resolve => setTimeout(resolve, 3000));
+    }, () => {
+        event.target.innerText = 'Failed';
+        return new Promise(resolve => setTimeout(resolve, 3000));
+    }).finally(() => {
+        event.target.innerText = 'Copy';
+    });
+}
