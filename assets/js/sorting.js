504ada2592101eab319aae038e76b31585b4840f
Edon <edon@valdman.works>
2023-05-13 12:14:13 -0400

    Create sorting.js
    Duplicated and reworked sorting algorithm in separate file for use with a "blanket" right-click event.

---
+++ assets/js/sorting.js
@@ -0,0 +1,40 @@
+/**
+ * @this {HTMLElement} DOM
+ * @param {Event} event 
+ */
+function sortRightClick(event) {
+    // element is right clicked
+    // check if it has the sort-asc attribute
+    const key = ev.target.getAttribute('sort-key');
+    if (event.target && key) {
+        event.preventDefault();
+        sortPlayerPool(event.target, key);
+    }
+}
+
+/**
+ * @param {EventTarget} target
+ * @param {string} sortKey 
+ */
+function sortPlayerPool(target, sortKey) {
+    const poolTable = document.getElementById('player-pool-table');
+    if (!poolTable) { return; }
+
+    let sortAsc = target.getAttribute('sort-asc');
+    if (!sortAsc) {
+        sortAsc = 'true';
+        target.setAttribute('sort-asc', 'true');
+    }
+
+    const sortedArr = all_players.toSorted((a, b) => {
+        return ((sortAsc === 'true') ? 1 : -1) * (a[sortKey] - b[sortKey]);
+    });
+
+    for (const child of poolTable.children) {
+        const id = child.firstElementChild.id.replace('-name-box', '');
+        const order = sortedArr.findIndex(p => p.id == id);
+        child.style.order = order;
+    }
+
+    event.target.setAttribute('sort-asc', !(sortAsc === 'true'));
+}
